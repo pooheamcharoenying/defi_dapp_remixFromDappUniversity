8504f016e4ec344bee3df9ab28375674d804db17
Pooh <pooh.e@projectlab.co.th>
2021-06-12 11:39:51 +0700

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,43 @@ class App extends Component {
   }
 
   async loadBlockchainData(dispatch) {
+    if (typeof window.ethereum !== 'undefined') {
+      const web3 = new Web3(window.ethereum)
+      const netId = await web3.eth.net.getId();
+      console.log('net id', netId);
+
+      const accounts = await web3.eth.getAccounts();
+      console.log('accounts', accounts);
+
+      const balance = await web3.eth.getBalance(accounts[0])
+      console.log('balance', balance);
+        this.setState({
+          account: accounts[0],
+          balance: balance,
+          web3: web3,
+        })
+
+        const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address );
+        const dbank = new web3.eth.Contract(dBank.abi, dBank.networks[netId].address );
+        const dBankAddress = dBank.networks[netId].address;
+        this.setState({
+          token: token,
+          dbank: dbank,
+          dBankAddress: dBankAddress,
+        })
+        console.log('dbank address', dBankAddress);
+
+        const tokenBalance = await token.methods.balanceOf(this.state.account).call()
+        console.log( 'token balance', web3.utils.fromWei(tokenBalance.toString()))
+
+        await this.checkDepositBalance();
+        await this.checkInterestBalance();
+        
+
+
+    } else {
+      window.alert('Please install MetaMask')
+    }
 
     //check if MetaMask exists
 
@@ -27,15 +64,99 @@ class App extends Component {
     //if MetaMask not exists push alert
   }
 
+  async checkDepositBalance() {
+    const depositBalance =  await this.state.dbank.methods.checkBalance().call({from:this.state.account});
+    this.setState({
+      depositBalance: depositBalance,
+    })
+    console.log('deposit balance', depositBalance)
+  }
+
+  async checkInterestBalance() {
+    const depositTime =  await this.state.dbank.methods.checkDepositTime().call({from:this.state.account});
+    console.log('deposit time', depositTime)
+    var date = new Date();
+    date = parseInt( date.getTime() /1000);
+    const holdTime = date - depositTime;
+    console.log('current time', date)
+    console.log('deposit time', depositTime)
+    console.log('hold time', holdTime)
+    console.log('deposit Valium', this.state.depositBalance )
+
+    const interestPerSecond = 31668017 * ( this.state.depositBalance / 1e16)
+    console.log('interest per second', interestPerSecond);
+
+    const interestBalance =(interestPerSecond * holdTime) /1e18;
+
+    this.setState({
+      interestBalance: interestBalance,
+    })
+    console.log('interest balance', interestBalance)
+  }
+
   async deposit(amount) {
     //check if this.state.dbank is ok
       //in try block call dBank deposit();
+      console.log('deposit amount', amount)
+      try {
+        await this.state.dbank.methods.deposit().send({value:amount.toString(), from:this.state.account})
+        await this.checkDepositBalance();
+      } catch(e) {
+        console.log('Error, deposit: ', e  )
+      }
   }
 
-  async withdraw(e) {
+  async withdraw(amount) {
     //prevent button from default click
     //check if this.state.dbank is ok
     //in try block call dBank withdraw();
+          //in try block call dBank deposit();
+    console.log('withdraw amount', amount)
+
+    try {
+      await this.state.dbank.methods.withdraw().send({from:this.state.account})
+      await this.checkDepositBalance();
+      await this.checkInterestBalance();
+    } catch(e) {
+      console.log('Error, withdraw: ', e  )
+    }
+  }
+
+  componentDidMount() {
+    console.log('mason mount')
+    var intervalId = setInterval(this.intervalLoop.bind(this), 1000);
+  }
+
+  intervalLoop() {
+    console.log('looping')
+    this.checkInterestBalance();    
+  } 
+
+  handleDepositChange(event) {
+    this.setState({
+      depositValue: event.target.value,
+    })
+  }
+
+  handleWithdrawChange(event) {
+    this.setState({
+      withdrawValue: event.target.value,
+    })
+  }
+
+  handleDepositSubmit(e) {
+    var amount = this.state.depositValue;
+    amount = amount * 10**18;
+    this.deposit(amount);
+    e.preventDefault();
+  }
+
+  handleWithdrawSubmit(event) {
+    var amount = this.state.withdrawValue;
+    amount = amount * 10**18;
+    this.withdraw(amount);
+
+    event.preventDefault();
   }
 
   constructor(props) {
@@ -46,7 +167,11 @@ class App extends Component {
       token: null,
       dbank: null,
       balance: 0,
-      dBankAddress: null
+      dBankAddress: null,
+      depositValue: "",
+      withdrawValue:"",
+      depositBalance:null,
+      interestBalance:null,
     }
   }
 
@@ -66,16 +191,34 @@ class App extends Component {
         </nav>
         <div className="container-fluid mt-5 text-center">
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
+          <h1> Welcome to Pooh Bank</h1>
+          <p>{ "Your address: " + this.state.account}</p>
           <br></br>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
-                {/*add Tab withdraw*/}
-              </Tabs>
+                <p> ยอดเงินฝาก ETH ในธนาคาร dbank: {this.state.depositBalance}</p>
+                <p> ยอดดอกเบี้ย DBC: {this.state.interestBalance}</p>
+                <h1> Deposit </h1> 
+                <form onSubmit={this.handleDepositSubmit.bind(this)}> 
+                  <label>
+                    Deposit Value:
+                    <input step="0.01" type="number"  type="text" value={this.state.depositValue} onChange={this.handleDepositChange.bind(this)} />
+                      </label>
+                      <input type="submit" value="Submit" />
+                </form>
+                <div style={{height:100}}></div>
+                <h1> Withdraw </h1>
+                <form onSubmit={this.handleWithdrawSubmit.bind(this)}>
+                  <label>
+                    Withdraw Value:
+                    <input  type="text" value={this.state.withdrawValue} onChange={this.handleWithdrawChange.bind(this)} />
+                  </label>
+                  <input type="submit" value="Submit" />
+                </form>
+
+              {/* <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+              </Tabs> */}
               </div>
             </main>
           </div>
--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -6,20 +6,35 @@ import "./Token.sol";
 contract dBank {
 
   //assign Token contract to variable
-
+  Token private token;
+   
   //add mappings
+  mapping(address => uint) public etherBalanceOf;
+  mapping(address => uint) public depositStart;
+  mapping(address => bool) public isDeposited;
+  
 
   //add events
+  event Deposit(address indexed user, uint etherAmount, uint timeStart);
+  event Withdraw(address indexed user, uint etherAmount, uint depositTime, uint interest);
 
   //pass as constructor argument deployed Token contract
-  constructor() public {
+  constructor(Token _token) public {
     //assign token deployed contract to variable
+    token = _token;
   }
 
   function deposit() payable public {
     //check if msg.sender didn't already deposited funds
+    require(isDeposited[msg.sender] == false, 'Error, deposit already active');
     //check if msg.value is >= than 0.01 ETH
+    require(msg.value>=1e16, 'Error, deposit must be >= 0.01 ETH');
 
+    etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] +  msg.value;
+    depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
+    isDeposited[msg.sender] =true;
+
+    emit Deposit(msg.sender, msg.value, block.timestamp);
     //increase msg.sender ether deposit balance
     //start msg.sender hodling time
 
@@ -29,19 +44,46 @@ contract dBank {
 
   function withdraw() public {
     //check if msg.sender deposit status is true
+    require(isDeposited[msg.sender]==true, 'Error, no previous deposit');
     //assign msg.sender ether deposit balance to variable for event
+    uint userBalance = etherBalanceOf[msg.sender];
 
     //check user's hodl time
-
-    //calc interest per second
+    uint depositTime =  block.timestamp - depositStart[msg.sender];
+    // calc interest per second in WEI per 0.01 Ether Deposited
+    //  10% interest rate per year
+    // 365.25 days in year = 31577600 seconds
+    // 1 year interest from 0.01 Ether is 0.001 Ether or 1e15 WEI
+    // therefore WEI per sencond per 0.01Ether = 1e15 / 31577600
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
     //calc accrued interest
+    uint interest = interestPerSecond * depositTime;
 
     //send eth to user
+    msg.sender.transfer(userBalance);
     //send interest in tokens to user
+    token.mint(msg.sender, interest); 
 
     //reset depositer data
+    etherBalanceOf[msg.sender] =0;    
+    depositStart[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
 
     //emit event
+    emit Withdraw(msg.sender, userBalance, depositTime, interest);
+  }
+
+  function checkBalance() external view returns(uint) {
+    uint myBalance = etherBalanceOf[msg.sender];
+    return myBalance;
+  }
+
+  function checkDepositTime() external view returns(uint) {
+    // require(isDeposited[msg.sender]==true, 'Error, no previous deposit');
+
+    uint timer =  depositStart[msg.sender];
+    // interest = interest+1;
+    return timer;
   }
 
   function borrow() payable public {
